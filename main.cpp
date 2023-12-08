a1cdefb68c48c45d4aed61875782816b170b3ed5
Aayush Karna <aayushkarna6191@gmail.com>
2023-12-08 15:21:14 +0545

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-using namspace std;
-int main(){
 
+void printWelcomeScreen()
+{
+  std::cout << "*******************************************" << std::endl;
+  std::cout << "*            Welcome to Sanduk            *" << std::endl;
+  std::cout << "*          Password Manager               *" << std::endl;
+  std::cout << "*******************************************" << std::endl;
+  std::cout << "*                                         *" << std::endl;
+  std::cout << "*                1) Create User           *" << std::endl;
+  std::cout << "*                2) Login                 *" << std::endl;
+  std::cout << "*                                         *" << std::endl;
+  std::cout << "*******************************************" << std::endl;
+  std::cout << std::endl;
+}
 
+int main()
+{
+  printWelcomeScreen();
+  return 0;
 }
